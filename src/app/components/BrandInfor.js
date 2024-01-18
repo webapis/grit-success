be11b5d13f0b4d0ca161cbb00968e5f3369b9d14
webapis <60083568+webapis@users.noreply.github.com>
2024-01-18 16:41:04 +0500

    Update BrandInfor.js

--- src/app/components/BrandInfor.js
+++ src/app/components/BrandInfor.js
@@ -4,7 +4,7 @@ import React from "react";
 import Chip from '@mui/material/Chip';
 export default function BrandInfor({ description, tag, title, keywords, href }) {
     return <div className="image-text-box" >
-        <img loading="lazy" src={`/brand-images-v2/${tag}1.JPG`} style={{ margin: 5, width: 280, height: 180, objectFit: 'contain' }} alt={`${title} kıyafet markası web sitesinin ana sayfa ekran görüntüsü`} />
+        <img loading="lazy" src={`/brand-images-v1/${tag}1.JPG`} style={{ margin: 5, width: 280, height: 180, objectFit: 'contain' }} alt={`${title} kıyafet markası web sitesinin ana sayfa ekran görüntüsü`} />
         <h3><a href={href} target="_blank">{title}</a> </h3>
         <div style={{ margin: 5 }}>{keywords && keywords.split(',').map((m, k) => <Chip variant={keywords.split(',').includes(m)?'filled':'outlined'} key={k} sx={{ marginRight: 1 }} size="small" label={m} />)}</div>
         {description}
