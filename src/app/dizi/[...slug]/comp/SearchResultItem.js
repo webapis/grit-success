5fa7a2870b8ddc10903e9f47526e6f1118584666
webapis <60083568+webapis@users.noreply.github.com>
2024-01-26 12:11:21 +0500

    Update SearchResultItem.js

--- src/app/dizi/[...slug]/comp/SearchResultItem.js
+++ src/app/dizi/[...slug]/comp/SearchResultItem.js
@@ -9,7 +9,7 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import IconButton from '@mui/material/IconButton';
 import Link from 'next/link';
 export default function SearchResultItem({ item }) {
-    const { "Row Labels": name, Website, ServiceName, Acyklama } = item
+    const { "Row Labels": name, Website, ServiceName, Acyklama,"Count of EpisodeNumber":count } = item
     const imageName = Website ? extractSubdomain(Website) : ""
     return <Card sx={{ maxWidth: 345 }}>
         <CardMedia
@@ -22,8 +22,11 @@ export default function SearchResultItem({ item }) {
             <Typography gutterBottom variant="h5" component="div">
                 {name}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography variant="body1" color="text.secondary">
                 {Acyklama}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" sx={{marginTop:2,textAlign:'end',fontStyle:"italic"}}>
+             Bu marka {count} bölümde sponsor oldu.
             </Typography>
         </CardContent>
         <CardActions>
