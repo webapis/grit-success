5840fe9ee567eaa7161eea59ee3b5cb880aef2d6
webapis <60083568+webapis@users.noreply.github.com>
2024-02-06 17:39:55 +0500

    Update Application.js

--- src/app/Application.js
+++ src/app/Application.js
@@ -16,7 +16,7 @@ export default function Application() {
     return <Box>
         <Box sx={{ width: { xs: '100%', md: '50%' }, margin: '0 auto', marginTop: 3, marginBottom: 3 }}>
 
-            <SearchBox searchAsYouType={false} />
+            <SearchBox searchAsYouType={false} placeholder='Kıyafet, Aksesuar, Takı...' />
         </Box>
         <Box sx={{ width: { xs: '100%', md: '50%' }, margin: '0 auto' }}>
             <InfiniteHits hitComponent={ListItem} showPrevious={false} />
