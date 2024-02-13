d05d604afad1e6ec53afb2ecf1771dc8947d0276
webapis <60083568+webapis@users.noreply.github.com>
2024-02-13 14:51:32 +0500

    ff

--- src/app/dizikiyafeti-dashboard/search/route.js
+++ src/app/dizikiyafeti-dashboard/search/route.js
@@ -1,14 +1,16 @@
 const algoliasearch = require('algoliasearch')
 const algoliaClient = algoliasearch('7JF244QSZZ', '9c4018bdcedb542cb7a0c9e5453aa7b0')
-const index = algoliaClient.initIndex('dizikiyefeti')
+
+
 
 export async function   POST(request) {
     const body = await request.json()
     debugger
-    const result = await index.saveObject(body,{autoGenerateObjectIDIfNotExist:true})
+    const { requests } = body;
+    console.log('body',body)
+    const results = await algoliaClient.search(requests);
+   
     debugger
-    return Response.json({ result })
+    return Response.json(results)
 
 }
-
-
