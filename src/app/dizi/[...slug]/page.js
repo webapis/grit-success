ad3db7ae3cc3efa070d461c39b0c3964d4861474
webapis <60083568+webapis@users.noreply.github.com>
2024-03-15 15:09:04 +0500

    Update page.js

--- src/app/dizi/[...slug]/page.js
+++ src/app/dizi/[...slug]/page.js
@@ -37,43 +37,43 @@ import Fuse from 'fuse.js'
 
 
 export default async function DiziPage({ params }) {
+return <div>Empry</div>
+    // const pages = await fs.readFile(path.join(process.cwd(), 'src/app/dizi-sponsoru/meta/pageMetaData.json'), 'utf8');
 
-    const pages = await fs.readFile(path.join(process.cwd(), 'src/app/dizi-sponsoru/meta/pageMetaData.json'), 'utf8');
+    // const pagesMetaData = JSON.parse(pages);
 
-    const pagesMetaData = JSON.parse(pages);
+    // const data = await fs.readFile(path.join(process.cwd(), 'src/app/dizi-sponsoru/page-data/dizisponsoru.json'), 'utf8');
+    // const pagesData = JSON.parse(data);
 
-    const data = await fs.readFile(path.join(process.cwd(), 'src/app/dizi-sponsoru/page-data/dizisponsoru.json'), 'utf8');
-    const pagesData = JSON.parse(data);
+    // const result = pagesMetaData.find(f => {
 
-    const result = pagesMetaData.find(f => {
+    //     const current = f.slug
+    //     const slug = params.slug[0]
+    //     const match = current === slug
 
-        const current = f.slug
-        const slug = params.slug[0]
-        const match = current === slug
+    //     return match
+    // })
 
-        return match
-    })
-
-    const fuse = new Fuse(pagesData, { keys: ['ServiceName', 'TVSeriesTitle', 'Tag', 'Name'], minMatchCharLength: 6 })
-
-
+    // const fuse = new Fuse(pagesData, { keys: ['ServiceName', 'TVSeriesTitle', 'Tag', 'Name'], minMatchCharLength: 6 })
 
 
-    if (result) {
-        const { pageTitle, search } = result
-        let results = fuse.search(search)
-        return <div>
-            <Container >
-                <SearchResultContainer data={results} pageTitle={pageTitle} />
-                <Grid sx={{ paddingTop: 5, paddingBottom: 5 }} container justifyContent="center">
-                    {/* {nextpages.length>0 && nextpages.map((m, i) => <Grid item key={i}> <Button size="small"   component={Link} href={`/${m.slug}`} startIcon={<SearchIcon />} variant='secondary' sx={{ fontSize: 16, textTransform: "lowercase", backgroundColor: "#eeeeee", margin: 1, borderRadius: 7, padding:2, textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}>{m.title}</Button> </Grid>)} */}
-                </Grid>
-            </Container>
 
-        </div>
-    } else {
-        return <div>Loading....</div>
-    }
+
+    // if (result) {
+    //     const { pageTitle, search } = result
+    //     let results = fuse.search(search)
+    //     return <div>
+    //         <Container >
+    //             <SearchResultContainer data={results} pageTitle={pageTitle} />
+    //             <Grid sx={{ paddingTop: 5, paddingBottom: 5 }} container justifyContent="center">
+    //                 {/* {nextpages.length>0 && nextpages.map((m, i) => <Grid item key={i}> <Button size="small"   component={Link} href={`/${m.slug}`} startIcon={<SearchIcon />} variant='secondary' sx={{ fontSize: 16, textTransform: "lowercase", backgroundColor: "#eeeeee", margin: 1, borderRadius: 7, padding:2, textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}>{m.title}</Button> </Grid>)} */}
+    //             </Grid>
+    //         </Container>
+
+    //     </div>
+    // } else {
+    //     return <div>Loading....</div>
+    // }
 
 
 
