bc5796208b417bda75c4a7febe3c24e67cf7bc61
webapis <60083568+webapis@users.noreply.github.com>
2024-03-15 15:02:13 +0500

    Update page.js

--- src/app/dizi/[...slug]/page.js
+++ src/app/dizi/[...slug]/page.js
@@ -1,48 +1,49 @@
 
-//import { promises as fs } from 'fs';
+import { promises as fs } from 'fs';
 
 import SearchResultContainer from '../../dizi-sponsoru/comp/SearchResultContainer';
 import { Container, Grid } from '@mui/material';
-//import path from 'path'
+import path from 'path'
 import Fuse from 'fuse.js'
-import pagesMetaData from '@/app/dizi-sponsoru/meta/pageMetaData.json';
-import pagesData from '@/app/dizi-sponsoru/page-data/dizisponsoru.json';
+import { title } from 'process';
+
+
 
 
 export async function generateMetadata({ params }) {
 
-   // const pages = await fs.readFile(path.join(process.cwd(), 'src/app/dizi/pageMetaData.json'), 'utf8');
-  //  const pagesData = JSON.parse(pages);
+    const pages = await fs.readFile(path.join(process.cwd(), 'src/app/dizi-sponsoru/meta/pageMetaData.json'), 'utf8');
+    const pagesData = JSON.parse(pages);
     debugger
-    const result = pagesMetaData.find(f => {
+    const result = pagesData.find(f => {
         const current = f.slug
         const slug = params.slug[0]
         const match = current === slug
 
         return match
     })
+    if (result) {
 
+        const { pageTitle } = result
+        return {
+            title: pageTitle
 
-    const {pageTitle} =result
-    return {
-        title:pageTitle
-
+        }
     }
 
- 
+
 }
 
 
 
 export default async function DiziPage({ params }) {
 
+    const pages = await fs.readFile(path.join(process.cwd(), 'src/app/dizi-sponsoru/meta/pageMetaData.json'), 'utf8');
 
- // const pages = await fs.readFile(path.join(process.cwd(), 'src/app/dizi/pageMetaData.json'), 'utf8');
+    const pagesMetaData = JSON.parse(pages);
 
-    //const pagesMetaData = JSON.parse(pages);
-
-   // const data = await fs.readFile(path.join(process.cwd(), 'src/app/dizi-sponsoru/page-data/dizisponsoru.json'),'utf8');
-   // const pagesData = JSON.parse(data);
+    const data = await fs.readFile(path.join(process.cwd(), 'src/app/dizi-sponsoru/page-data/dizisponsoru.json'), 'utf8');
+    const pagesData = JSON.parse(data);
 
     const result = pagesMetaData.find(f => {
 
@@ -52,30 +53,29 @@ export default async function DiziPage({ params }) {
 
         return match
     })
- 
-    const fuse = new Fuse(pagesData,{keys:['ServiceName','TVSeriesTitle','Tag','Name'], minMatchCharLength: 6})
 
-   
-    
+    const fuse = new Fuse(pagesData, { keys: ['ServiceName', 'TVSeriesTitle', 'Tag', 'Name'], minMatchCharLength: 6 })
 
-      if(result){
-        const {  pageTitle, search }=result
+
+
+
+    if (result) {
+        const { pageTitle, search } = result
         let results = fuse.search(search)
         return <div>
-      <Container >
-          <SearchResultContainer data={results} pageTitle={pageTitle} />
-          <Grid sx={{ paddingTop: 5, paddingBottom: 5 }} container justifyContent="center">
-              {/* {nextpages.length>0 && nextpages.map((m, i) => <Grid item key={i}> <Button size="small"   component={Link} href={`/${m.slug}`} startIcon={<SearchIcon />} variant='secondary' sx={{ fontSize: 16, textTransform: "lowercase", backgroundColor: "#eeeeee", margin: 1, borderRadius: 7, padding:2, textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}>{m.title}</Button> </Grid>)} */}
-          </Grid>
-      </Container>
+            <Container >
+                <SearchResultContainer data={results} pageTitle={pageTitle} />
+                <Grid sx={{ paddingTop: 5, paddingBottom: 5 }} container justifyContent="center">
+                    {/* {nextpages.length>0 && nextpages.map((m, i) => <Grid item key={i}> <Button size="small"   component={Link} href={`/${m.slug}`} startIcon={<SearchIcon />} variant='secondary' sx={{ fontSize: 16, textTransform: "lowercase", backgroundColor: "#eeeeee", margin: 1, borderRadius: 7, padding:2, textDecoration: 'none', '&:hover': { textDecoration: 'underline' } }}>{m.title}</Button> </Grid>)} */}
+                </Grid>
+            </Container>
 
-  </div>
-      }else{
-        return <div>Loading.......</div>
-      }
-   
-      
-    
+        </div>
+    } else {
+        return <div>Loading....</div>
+    }
+
+
 
 
 }
