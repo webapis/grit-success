7bb82c0b977cb2c8ea64fe853736d282f203dd6d
webapis <60083568+webapis@users.noreply.github.com>
2024-03-15 09:36:21 +0500

    gg

--- src/app/dizi-sponsoru/Application.js
+++ src/app/dizi-sponsoru/Application.js
@@ -1,46 +1,32 @@
 'use client'
 import React from 'react';
-
-import {
-
-    SearchBox, Stats, CurrentRefinements
-} from 'react-instantsearch';
-import PersistentDrawerLeft from './drawer';
-import AlgoliaProvider from './AlgoliaProvider'
-import SearchResultContainer from './[...slug]/comp/SearchResultContainer';
-
-import { Box, Grid, Typography } from '@mui/material';
-
-export default function Application({initialUiState}) {
+import { Typography, Grid, Container } from '@mui/material';
+import SponsorView from '@/app/dizi-sponsoru/[...slug]/comp/SponsorView';
+import data from '@/app/dizi-sponsoru/page-data/dizisponsoruMenu.json'
+//import Drawer from './drawer/index'
+const arrayData = Object.entries(data)
 
 
+export default function Application() {
 
-    return <AlgoliaProvider initialUiState={initialUiState}>
-        <PersistentDrawerLeft>
+    return <Container >
+
+        <Typography variant='h4' textAlign='center' sx={{ marginTop: 2 }}>Dizi Sponsorları</Typography>
 
 
-            <Box>
-                <Box sx={{ width: { xs: '100%', md: '50%' }, margin: '0 auto', marginTop: 0 }}>
+        <Grid container gap={1} style={{display:'flex',justifyContent:'center'}}>
+            {arrayData.sort((a,b)=> b[1].toplamSponsor-a[1].toplamSponsor).map((m,i) => {
+                const title = m[0]
+                const content = m[1]
+                return <Grid key={i} item xs={12} md={3}> <SponsorView title={title} content={content} /></Grid>
 
-                    <SearchBox searchAsYouType={false} placeholder='Dizi, Sponsor, Sponsorluk...' />
-                </Box>
-                <Typography variant='h5' textAlign='center' sx={{marginTop:2}}>Türk dizilerin sponsor markaları</Typography>
-                <Box sx={{ width: { xs: '100%', md: '50%' }, margin: '0 auto' }}>
-                    <CurrentRefinements style={{ padding: 5 }} />
-                    {/* <Stats style={{ margin: 5 }} /> */}
+            })}
+        </Grid>
 
 
 
 
-          <SearchResultContainer /> 
-
-
-                </Box>
-
-            </Box>
-        </PersistentDrawerLeft>
-    </AlgoliaProvider>
-
+    </Container>
+  
 }
-
 
---
+++ src/app/dizi-sponsoru/[...slug]/comp/SponsorView.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from 'next/link';
+export default function SponsorView({title ,content }) {
+    const {tag,toplamSponsor}=content
+
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                sx={{ height: 170 }}
+                image={`/dizi/cover-image/${tag}.jpg`}
+                title={`${title} Dizi Sponsorları.`}
+            />
+
+            <CardContent>
+                <Box  style={{display:'flex', alignItems:'baseline', justifyContent:'space-between'}}>
+                <Typography  variant="h5" component="div" style={{ textTransform: 'capitalize' }}>
+                    {title}
+                </Typography>
+              
+                </Box>
+         
+                <Typography variant="body2" color="text.secondary" style={{ textTransform: 'capitalize' }}>
+                    {title} Dizi Sponsorları.
+                </Typography>
+                <Typography  variant="caption" color="text.secondary" component="div" style={{ textTransform: 'capitalize',marginTop:4 }}>
+                   Sponsor sayısı: {toplamSponsor}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" component={Link} href={`/dizi-sponsoru/${tag}-dizi-sponsorları`} target='_blank'>sayfaya git</Button>
+            </CardActions>
+        </Card>
+    );
+}
