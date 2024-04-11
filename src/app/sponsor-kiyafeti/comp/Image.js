f01f895fd1895b8dba26107078f1f4c14d6884ac
Serdar Ashirov <webapis@users.noreply.github.com>
2024-04-11 11:52:49 +0500

    Update Image.js

--- src/app/sponsor-kiyafeti/comp/Image.js
+++ src/app/sponsor-kiyafeti/comp/Image.js
@@ -8,8 +8,8 @@ export default function Image({ image, title, link, marka, price, currency }) {
 
     return <Box container sx={{ width:{ xs: 150, md: 200},height:{xs:200,md:300} }}>
         <Box item xs={12} component={Link} href={link} target="_blank">
-            <img   
-        src={'https://ik.imagekit.io/mumrjdehaou/'+image[0]} loading="lazy" style={{ maxWidth: '100%', height: '100%',objectFit:'contain' }}  />
+            {/* <img   
+        src={'https://ik.imagekit.io/mumrjdehaou/'+image[0]} loading="lazy" style={{ maxWidth: '100%', height: '100%',objectFit:'contain' }}  /> */}
         </Box>
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Typography sx={{fontSize:14,fontFamily:'inherit'}}>{marka}</Typography>
