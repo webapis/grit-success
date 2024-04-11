833fb4f58781ef806f36d953fb2c0eaf1f4d7b74
Serdar Ashirov <webapis@users.noreply.github.com>
2024-04-11 12:16:40 +0500

    Update Image.js

--- src/app/sponsor-kiyafeti/comp/Image.js
+++ src/app/sponsor-kiyafeti/comp/Image.js
@@ -31,7 +31,7 @@ export default function Image({ image, title, link, marka, price, currency }) {
         }
       }, []);
     return <Box container >
-        <Box item xs={12} component={Link} href={link} target="_blank">
+        <Box item xs={12}  target="_blank">
           {image && image[0] &&             <img    ref={imageEl}
         data-src={'https://ik.imagekit.io/mumrjdehaou/'+image[0]} src={placeholder} style={{ maxWidth: '100%', height: 'auto' }}  />}
 
@@ -41,7 +41,7 @@ export default function Image({ image, title, link, marka, price, currency }) {
             <Typography sx={{fontSize:12,fontFamily:'inherit'}}>{price} {currency}</Typography>
         </Box>
         <Box>
-            <Typography  component={Link} href={link} target="_blank" variant="caption" style={{ width: '100%', wordWrap: 'break-word', textDecoration: 'none', color:'inherit',fontFamily:'inherit' }}>{title}</Typography>
+            <Typography   variant="caption" style={{ width: '100%', wordWrap: 'break-word', textDecoration: 'none', color:'inherit',fontFamily:'inherit' }}>{title}</Typography>
         </Box>
     </Box>
 }
