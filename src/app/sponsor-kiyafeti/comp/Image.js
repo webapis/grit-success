31f7f4ef61b4a4d597c60d06a58a840e1d8fd460
Serdar Ashirov <webapis@users.noreply.github.com>
2024-04-11 13:28:51 +0500

    Update Image.js

--- src/app/sponsor-kiyafeti/comp/Image.js
+++ src/app/sponsor-kiyafeti/comp/Image.js
@@ -12,15 +12,15 @@ export default function Image({ image, title, link, marka, price, currency }) {
     <Box sx={{ width: { xs: 150, md: 200 }, height: { xs: 200, md: 300 } }}>
       {
         image && image[0] && <img
-          src={'https://ik.imagekit.io/mumrjdehaou/' + image[0]} loading="lazy" style={{ maxWidth: '100%', height: '100%', objectFit: 'contain' }} />
+          src={'https://ik.imagekit.io/mumrjdehaou/' + image[0]} loading="lazy" style={{ maxWidth: '100%', height: '100%', objectFit: 'contain',backgroundColor:'#f5f5f5' }} />
       }
 
     </Box>
-    <Box sx={{ display: 'flex', justifyContent: 'space-between',width: { xs: 150, md: 200 } }}>
+    <Box sx={{ display: 'flex', justifyContent: 'space-between',width: { xs: 140, md: 200 } }}>
       <Typography sx={{ fontSize: 14, fontFamily: 'inherit' }}>{marka}</Typography>
-      <Typography sx={{ fontSize: 12, fontFamily: 'inherit' }}>{price} {currency}</Typography>
+      <Typography sx={{ fontSize: 11, fontFamily: 'inherit' }}>{price} {currency}</Typography>
     </Box>
-    <Box sx={{width: { xs: 150, md: 200 }}}>
+    <Box sx={{width: { xs: 140, md: 200 }}}>
       <Typography variant="caption" style={{ width: '100%', wordWrap: 'break-word', textDecoration: 'none', color: 'inherit', fontFamily: 'inherit' }}>{title}</Typography>
     </Box>
   </Box>
