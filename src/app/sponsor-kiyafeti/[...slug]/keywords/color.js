0167fe8b541ee98bbc5ad3fb5c40310c6f39ff50
Serdar Ashirov <webapis@users.noreply.github.com>
2024-04-17 12:44:23 +0500

    Update color.js

--- src/app/sponsor-kiyafeti/[...slug]/keywords/color.js
+++ src/app/sponsor-kiyafeti/[...slug]/keywords/color.js
@@ -5,6 +5,6 @@ const colors = {
     fuşya: { group: 'boyu', total: 0 },
     sarı: { group: 'boyu', total: 0 },
     kahve: { group: 'boyu', total: 0 },
-    turuncu: { group: 'boyu', total: 0 },
+    turuncu: { group: 'boyu', total: 0 },  yeşil: { group: 'boyu', total: 0 },
 }
 export default colors
