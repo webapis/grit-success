ced6667521f968f0e3c8608a1f2765b3f026001b
Serdar Ashirov <webapis@users.noreply.github.com>
2024-04-22 12:42:02 +0500

    Update SelectedDiziChip.js

--- src/app/dizikiyafeti/comps/SelectedDiziChip.js
+++ src/app/dizikiyafeti/comps/SelectedDiziChip.js
@@ -3,5 +3,5 @@ import { Chip,Box } from "@mui/material";
 
 export default function SelectedDiziChip({category}){
 
-    return  <Box sx={{display:'flex', justifyContent:'center',margin:2}}><Chip component='h1' size="small" color="primary" variant="outlined" label={category} onDelete={()=>{window.location.replace('/') }}/></Box> 
+    return  <Box sx={{display:'flex', justifyContent:'center',margin:2}}><Chip component='h1' size="small" color="primary" variant="outlined" label={category} onDelete={()=>{window.location.replace('/dizikiyafeti') }}/></Box> 
 }
