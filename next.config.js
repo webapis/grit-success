2f56ade71f910cb8208cb2b7c7e7752c189eff59
Serdar Ashirov <webapis@users.noreply.github.com>
2024-07-14 12:41:05 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/sayfa/',
+        destination: '/sayfa/1',
+        permanent: true,
+      },
+      {
+        source: '/sayfa',
+        destination: '/sayfa/1',
+        permanent: true,
+      },
+    ]
+  },
   output: 'export',
 }
 
