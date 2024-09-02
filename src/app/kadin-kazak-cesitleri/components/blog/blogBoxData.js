3c2dfb5bcfd5b950cf68b1c1bb826542c6837fc5
Serdar Ashirov <webapis@users.noreply.github.com>
2024-09-02 16:44:23 +0500

    s

--- src/app/kadin-kazak-cesitleri/components/blog/blogBoxData.js
+++ src/app/kadin-kazak-cesitleri/components/blog/blogBoxData.js
@@ -25,6 +25,7 @@ const data = [{
     <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
     <ul>
         <li><strong>Yuvarlak yaka kazak:</strong> Bisiklet yaka kazak için kullanılan bir diğer terimdir.</li>
+        <li><strong>Sıfır yaka kazak:</strong> Bisiklet yaka kazak için kullanılan bir diğer terimdir.</li>
         <li><strong>Crew neck (İngilizce):</strong> Bisiklet yaka kazağın İngilizce karşılığıdır.</li>
     </ul>`,
     alt: 'Bisiklet yaka kazak',
@@ -133,6 +134,294 @@ const data = [{
     alt: 'Kazak Elbise',
     link: 'kadin-kazak-cesitleri/kazak-elbise',
     src: 'kadin-kazak/sweater-dress.jpg'
+},
+
+{
+    title: 'Ajurlu Kazak',
+    description: ` <p>
+        <strong>Ajurlu kazak</strong>, örgü veya dantel desenleriyle dekoratif delikler veya açıklıklar içeren bir kazak türüdür. "Ajur" terimi, Fransızca "ajour" kelimesinden gelir ve "açık" anlamına gelir, bu nedenle ajurlu kazaklar genellikle hafif ve hava geçiren bir yapıya sahiptir. Ajurlu kazaklar, desenlerin oluşturulmasında çeşitli teknikler kullanarak dokular ve desenler oluşturur, bu da kazaklara estetik bir görünüm kazandırır. Bu tür kazaklar, hem yaz hem de kış modasında kullanılabilir ve şık bir dokunuş sağlar.
+    </p>
+    <p>
+        Ajurlu kazaklar, genellikle ince ipliklerden yapılır ve sıcaklık sağlamakla birlikte hafif bir hava geçişine izin verir. Dantel, dikiş veya örgü teknikleriyle yapılan ajur detayları, kazakları diğerlerinden ayıran özel bir özellik olarak öne çıkar.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Dantel kazak:</strong> Ajurlu kazaklarla benzer şekilde açık veya delikli desene sahip kazakları ifade eder.</li>
+        <li><strong>Lace sweater (İngilizce):</strong> Ajurlu kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Ajurlu Kazak',
+    link: 'kadin-kazak-cesitleri/ajurlu-elbise',
+    src: 'kadin-kazak/lace-sweater.jpg'
+}
+,
+
+{
+    title: 'Saç Örgülü Kazak',
+    description: `<p>
+        <strong>Saç örgülü kazak</strong>, yüzeyinde belirgin bir saç örgüsü deseni bulunan bir kazak türüdür. "Saç örgüsü" terimi, örgüde oluşturulan ve bir saçı andıran desenleri ifade eder; genellikle bu desenler, örgü ipliklerinin çapraz ve düzenli bir şekilde örülmesiyle meydana gelir. Saç örgülü kazaklar, kalın ve sıcak tutan bir yapı sunar ve özellikle kış aylarında tercih edilir. Bu tür kazaklar, hem klasik hem de modern stiller için uygun olup, genellikle kalın yün veya akrilik ipliklerden üretilir.
+    </p>
+    <p>
+        Saç örgüsü deseni, kazaklara hem şık bir görünüm kazandırır hem de ekstra bir sıcaklık sağlar. Bu tür kazaklar, genellikle casual giyimde veya daha şık kombinlerde rahatlıkla kullanılabilir. Saç örgülü kazaklar, soğuk hava koşullarında konforlu bir seçenek sunar ve çeşitli renk ve kesim seçenekleriyle üretilir.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Saç örgüsü deseni:</strong> Saç örgülü kazaklar için kullanılan bir diğer terimdir.</li>
+        <li><strong>Cable knit sweater (İngilizce):</strong> Saç örgülü kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Saç Örgülü Kazak',
+    link: 'kadin-kazak-cesitleri/sac-orgulu-kazak',
+    src: 'kadin-kazak/cable-knit-sweater.jpg'
+}
+,
+
+{
+    title: 'Oversize kazak',
+    description: `<p>
+        <strong>Oversize kazak</strong>, normalden daha büyük ve geniş kesime sahip olan bir kazak türüdür. "Oversize" terimi, bu tür giysilerin bilinen bedenden daha geniş ve rahat bir şekilde tasarlandığını ifade eder. Oversize kazaklar, rahatlık ve bol bir görünüm sunar, bu da onları hem günlük hem de şık kombinler için popüler bir seçim yapar.
+    </p>
+    <p>
+        Bu kazaklar genellikle bol kesimli olup, omuzlardan bele kadar geniş bir forma sahiptir. Oversize kazaklar, katmanlı giyinme trendine uygun olarak, tişört, gömlek veya bluzların üzerine rahatça giyilebilir. Yüksek bel pantolonlar veya taytlarla kombinlendiğinde, stil sahibi bir görünüm sağlar. Özellikle soğuk havalarda konforlu bir sıcaklık sunar.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Geniş kesim kazak:</strong> Oversize kazaklarla benzer şekilde büyük ve rahat bir kesimi ifade eder.</li>
+         <li><strong>Salaş kazak:</strong> Oversize kazaklarla benzer şekilde büyük ve rahat bir kesimi ifade eder.</li>
+        <li><strong>Oversized sweater (İngilizce):</strong> Oversize kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Oversize kazak',
+    link: 'kadin-kazak-cesitleri/oversize-kazak',
+    src: 'kadin-kazak/oversized-sweater.jpg'
+},
+
+{
+    title: 'Madonna yaka kazak',
+    description: `<p>
+        <strong>Madonna yaka kazak</strong>, Madonna'nın 1980'ler ve 1990'lar modasında etkili olduğu dönemde popüler hale gelen, belirgin ve şık bir yaka tasarımına sahip kazak türüdür. Madonna yaka, genellikle geniş, dökümlü ve düşük bir yaka kısmıyla tanınır. Bu yaka tasarımı, omuzları ve dekolteyi vurgular ve kazaklara sofistike bir hava katar.
+    </p>
+    <p>
+        Madonna yaka kazaklar genellikle şık ve feminen bir görünüm sunar, bu yüzden özel günlerde veya akşam etkinliklerinde tercih edilir. Yaka kısmı, genellikle kazakla aynı malzemeden yapılır ve kazakla uyumlu bir şekilde tasarlanır. Bu tasarım, hem sıcaklık sağlamak hem de şık bir görünüm oluşturmak amacıyla kullanılır.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Düşük yaka kazak:</strong> Madonna yaka kazaklarla benzer şekilde geniş ve düşük yaka tasarımına sahip kazakları ifade eder.</li>
+        <li><strong>Off-the-shoulder sweater (İngilizce):</strong> Madonna yaka kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Madonna yaka kazak',
+    link: 'kadin-kazak-cesitleri/madonna-yaka',
+    src: 'kadin-kazak/off-the-shoulder-sweater.jpg'
+},
+{
+    title: 'Yarım balıkçı yaka kazak',
+    description: `<p>
+        <strong>Yarım balıkçı yaka kazak</strong>, balıkçı yaka kazakların kısaltılmış bir versiyonudur. Balıkçı yaka kazaklar, yüksek ve sıkı bir yakaya sahipken, yarım balıkçı yaka kazaklarda yaka kısmı daha kısa ve hafifçe düşük olacak şekilde tasarlanır. Bu tasarım, boğazı tamamen kapatmadan daha rahat bir giyim deneyimi sunar, yine de sıcaklık sağlar ve stil sahibi bir görünüm sunar.
+    </p>
+    <p>
+        Yarım balıkçı yaka kazaklar, genellikle kış aylarında tercih edilir, ancak hafif yapıları sayesinde mevsim geçişlerinde de rahatlıkla kullanılabilir. Üzerine mont veya ceket giyildiğinde, yaka kısmı biraz dışarıda kalacak şekilde tasarlanır, bu da şık bir detay oluşturur. Bu tür kazaklar, hem casual hem de daha şık kombinler için uygundur.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Kısa balıkçı yaka:</strong> Yarım balıkçı yaka kazaklar için kullanılan başka bir terimdir.</li>
+        <li><strong>Mock neck sweater (İngilizce):</strong> Yarım balıkçı yaka kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Yarım balıkçı yaka kazak',
+    link: 'kadin-kazak-cesitleri/yarim-balikci-yaka',
+    src: 'kadin-kazak/mock-neck-sweater.jpg'
+},
+,
+{
+    title: 'Kare yaka kazak',
+    description: ` <p>
+        <strong>Kare yaka kazak</strong>, yaka kısmında belirgin bir kare formu olan bir kazak türüdür. Kare yaka tasarımı, genellikle düz ve keskin köşelere sahip olup, omuzları ve boynu belirgin bir şekilde vurgular. Bu yaka türü, kıyafetlere modern ve şık bir görünüm kazandırır.
+    </p>
+    <p>
+        Kare yaka kazaklar, genellikle çeşitli malzemelerden yapılabilir ve rahat bir giyim deneyimi sunar. Bu tasarım, hem gündelik hem de daha resmi kombinler için uygun olup, çeşitli renk ve desen seçenekleriyle üretilir. Kare yaka, özellikle kış modasında dikkat çekici bir detay olarak öne çıkar.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Kare yaka:</strong> Kare şeklinde bir yaka tasarımını ifade eder.</li>
+        <li><strong>Square neck sweater (İngilizce):</strong> Kare yaka kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Kare yaka kazak',
+    link: 'kadin-kazak-cesitleri/kare-yaka',
+    src: 'kadin-kazak/square-neck-sweater.jpg'
+},
+
+{
+    title: 'Dik yaka kazak',
+    description: `<p>
+        <strong>Dik yaka kazak</strong>, dik bir şekilde tasarlanmış, genellikle boğazı veya boynu kaplayan bir yaka türüdür. Bu yaka tasarımı, kazakların daha modern ve şık bir görünüm kazanmasını sağlar. Dik yaka, kazak üzerinde yukarıya doğru dik bir biçimde yer alır ve bu da boğazı veya yakayı daha belirgin hale getirir.
+    </p>
+    <p>
+        Dik yaka kazaklar genellikle şık ve stil sahibi bir görünüm sunar ve kış aylarında rahatlıkla tercih edilir. Yaka kısmı, çoğunlukla kazakla aynı malzemeden yapılır ve bu da tutarlı bir görünüm sağlar. Ayrıca, dik yaka kazaklar, soğuk havalarda ekstra koruma sağlar ve bazen dış giysilerle birlikte kombinlenir.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Yüksek yaka:</strong> Dik yaka ile benzer şekilde boğazı veya boynu kaplayan bir tasarımı ifade eder.</li>
+        <li><strong>Stand-up collar sweater (İngilizce):</strong> Dik yaka kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Dik yaka kazak',
+    link: 'kadin-kazak-cesitleri/dik-yaka',
+    src: 'kadin-kazak/stand-up-collar-sweater.jpg'
+}
+,
+
+{
+    title: 'Kayık yaka kazak',
+    description: `<p>
+        <strong>Kayık yaka kazak</strong>, geniş ve düşük bir yaka tasarımına sahip bir kazak türüdür. Kayık yaka, yaka kısmının omuzları ve boğazı vurgulayan, geniş bir şekilde tasarlandığı bir yaka modelidir. Genellikle yaka kısmı omuzları ve bazen kolları da kapsayacak şekilde genişletilmiş olarak tasarlanır, bu da rahat ve şık bir görünüm sağlar.
+    </p>
+    <p>
+        Kayık yaka kazaklar, zarif ve feminen bir görünüm sunar, bu yüzden özel günlerde veya akşam etkinliklerinde tercih edilebilir. Bu tasarım, genellikle omuzları sergileyen ve boğazın bir kısmını kaplayan bir yapıya sahiptir, bu da kazaklara hafif ve rahat bir hava katmaktadır.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Boat neck sweater (İngilizce):</strong> Kayık yaka kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Kayık yaka kazak',
+    link: 'kadin-kazak-cesitleri/kayik-yaka',
+    src: 'kadin-kazak/boat-neck-sweater.jpg'
+}
+,
+
+{
+    title: 'Gömlek yaka kazak',
+    description: `<p>
+        <strong>Gömlek yaka kazak</strong>, bir kazak üzerine gömlek yaka detayının eklenmiş olduğu bir tasarımdır. Bu tür kazaklarda, kazak kısmı genellikle klasik bir kazak tasarımına sahipken, yaka kısmı gömlek yakası şeklinde tasarlanır. Gömlek yaka, genellikle daha şık ve resmi bir hava katmak için kullanılır ve kazakla birleştiğinde hem rahat hem de şık bir görünüm sağlar.
+    </p>
+    <p>
+        Gömlek yaka kazaklar, genellikle ofis ortamları veya resmi etkinlikler için tercih edilir. Yaka kısmı, gömleklerden alışık olduğumuz klasik yaka detaylarına sahip olup, bu da kazakları daha sofistike bir hale getirir. Genellikle gömlek yaka, kazak ile uyumlu bir şekilde tasarlanır ve kıyafete zarif bir dokunuş ekler.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Shirt collar sweater (İngilizce):</strong> Gömlek yaka kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Gömlek yaka kazak',
+    link: 'kadin-kazak-cesitleri/gomlek-yaka',
+    src: 'kadin-kazak/shirt-collar-sweater.jpg'
+}
+,
+
+{
+    title: 'Salaş kazak',
+    description: `<p>
+        <strong>Salaş kazak</strong>, genellikle bol ve rahat bir kesime sahip olan bir kazak türüdür. Salaş kazaklar, vücuda yapışmayan, geniş ve dökümlü bir tasarıma sahiptir. Bu tür kazaklar, rahat bir giyim deneyimi sunar ve genellikle günlük kullanım için idealdir.
+    </p>
+    <p>
+        Salaş kazakların karakteristik özellikleri arasında geniş omuzlar, bol kollar ve rahat bir vücut kesimi bulunur. Bu tasarım, hem konforu artırır hem de şık ve modern bir görünüm sağlar. Salaş kazaklar, hem sıcaklık sağlar hem de rahat hareket imkanı sunar, bu yüzden soğuk havalarda veya rahat bir gün geçirmek istendiğinde tercih edilir.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Oversize kazak:</strong> Salaş kazaklarla benzer şekilde, büyük ve rahat bir kesime sahip kazakları ifade eder.</li>
+        <li><strong>Loose fit sweater (İngilizce):</strong> Salaş kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Salaş kazak',
+    link: 'kadin-kazak-cesitleri/salas-kazak',
+    src: 'kadin-kazak/salas-kazak.jpg'
+}
+,
+
+{
+    title: 'Baklava Desenli Kazak',
+    description: `<p>
+        <strong>Baklava desenli kazak</strong>, yüzeyinde klasik baklava şekillerini andıran desenlere sahip bir kazak türüdür. Baklava deseni, genellikle çapraz ve birbirini takip eden romboid (baklava şeklinde) figürlerden oluşur ve bu desen, kazaklara belirgin ve estetik bir görünüm kazandırır. Bu desen, kazakların hem şıklığını artırır hem de onları daha dikkat çekici hale getirir.
+    </p>
+    <p>
+        Baklava desenli kazaklar, genellikle kış mevsiminde sıcaklık sağlayan ve aynı zamanda şık bir görünüm sunan kıyafetler olarak tercih edilir. Desen, kazak üzerinde hafif kabartma veya dokusal bir etkisi olabilir, bu da hem estetik hem de işlevsel bir değer katar.
+    </p>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Diamond pattern sweater:</strong> Baklava desenli kazakların İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Baklava Desenli Kazak',
+    link: 'kadin-kazak-cesitleri/baklava-desenli',
+    src: 'kadin-kazak/baklava-desenli-kazak.jpg'
+}
+,
+
+{
+    title: 'Omzu açık kazak',
+    description: ` <p>
+        <strong>Omzu açık kazak</strong>, tasarımında omuz kısmının açıkta olduğu ve genellikle omuzları sergileyen bir kazak türüdür. Bu stil, modern ve şık bir görünüm sunar ve genellikle daha rahat veya şık bir hava yaratmak amacıyla tercih edilir.
+    </p>
+    <p>
+        Omzu açık kazaklar, genellikle şu özelliklere sahiptir:
+    </p>
+    <ul>
+        <li><strong>Omuz Detayları:</strong> Kazak, omuzları sergileyecek şekilde tasarlanır. Bu detay bazen sadece bir omuzu açık bırakırken, bazen de her iki omuzda açık olabilir.</li>
+        <li><strong>Tasarım:</strong> Omuz kısmı açık olan kazaklar, şıklığı artırmak amacıyla çeşitli detaylarla süslenebilir. Örneğin, dikiş detayları, fırfırlar veya danteller gibi ek süslemeler içerebilir.</li>
+        <li><strong>Kullanım Alanları:</strong> Bu tür kazaklar, özellikle yaz sonu ve sonbahar mevsimlerinde, hafif serin hava koşullarında rahat bir seçenek sunar. Ayrıca, özel günlerde veya akşam etkinliklerinde şık bir görünüm sağlayabilir.</li>
+        <li><strong>Tarz:</strong> Omzu açık kazaklar, hem gündelik hem de özel kıyafetler için uygun olabilir. Genellikle rahat bir görünüm sunduğu için rahatlıkla kombinlenebilir.</li>
+    </ul>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Off-shoulder sweater (İngilizce):</strong> Omzu açık kazağın İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Omzu açık kazak',
+    link: 'kadin-kazak-cesitleri/omzu-acik',
+    src: 'kadin-kazak/off-shoulder-sweater.jpg'
+}
+
+,
+
+{
+    title: 'Crop kazak',
+    description: `<p>
+        <strong>Crop kazak</strong> (veya kısa kazak), normalden daha kısa ve genellikle karın kısmını veya bel kısmını sergileyen bir kazak türüdür. Bu tasarım, kıyafete genç, modern ve dinamik bir görünüm kazandırır. Crop kazaklar, hem rahat hem de şık bir seçenek sunar ve farklı stiller ve mevsimlere uyacak şekilde tasarlanabilir.
+    </p>
+    <p>
+        <strong>Özellikleri:</strong>
+    </p>
+    <ul>
+        <li><strong>Kısa Kesim:</strong> Crop kazaklar, bel hizasında veya karın kısmının biraz üzerinde sonlanacak şekilde tasarlanır. Bu kısa kesim, rahat bir görünüm sağlar ve vücudu sergileyebilir.</li>
+        <li><strong>Tasarım:</strong> Crop kazaklar çeşitli stillerde ve detaylarda olabilir. Klasik, oversize, yumuşak dokular veya desenlerle süslenmiş olabilir. Genellikle rahat bir kesim sunar.</li>
+        <li><strong>Kullanım Alanları:</strong> Bu tür kazaklar, özellikle yaz sonu ve sonbahar mevsimlerinde popülerdir. Ayrıca, sıcak hava koşullarında rahat bir seçenek olarak tercih edilebilir. Hem günlük kullanımda hem de özel etkinliklerde şık bir görünüm sunar.</li>
+        <li><strong>Stil:</strong> Crop kazaklar, yüksek bel pantolonlar veya eteklerle kombinlenerek modern ve şık bir görünüm oluşturabilir. Ayrıca, spor veya rahat stil kıyafetlerle de kullanılabilir.</li>
+    </ul>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Kısa Kazak:</strong> Crop kazakla benzer anlamda kullanılabilir.</li>
+        <li><strong>Crop Top Sweater (İngilizce):</strong> Crop kazakların İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Crop kazak',
+    link: 'kadin-kazak-cesitleri/crop',
+    src: 'kadin-kazak/crop-top-sweater.jpg'
+},
+
+{
+    title: 'Jakarlı kazak',
+    description: `<p>
+        <strong>Jakarlı kazak</strong>, yüzeyinde desen veya motiflerin dokuma sırasında kumaşa entegre edildiği bir kazak türüdür. Jakar dokuma tekniği, kumaşın içine desenlerin işlenmesini sağlar, bu da kazaklara estetik ve şık bir görünüm kazandırır.
+    </p>
+    <p>
+        <strong>Özellikleri:</strong>
+    </p>
+    <ul>
+        <li><strong>Desen ve Motifler:</strong> Jakarlı kazaklarda desenler veya motifler kumaşın her yerine eşit şekilde dağılmıştır. Desenler, dokuma sırasında ipliklerin değiştirildiği bir yöntemle oluşturulur, bu da kazaklara derinlik ve detay kazandırır.</li>
+        <li><strong>Dokuma Tekniği:</strong> Jakar dokuma tekniği, desenlerin doğrudan kumaşın içine işlenmesiyle gerçekleştirilir. Bu teknik, hem tek renkli hem de çok renkli desenlerin oluşturulmasına olanak tanır.</li>
+        <li><strong>Kullanım Alanları:</strong> Jakarlı kazaklar genellikle şık ve klasik bir görünüm sunar. Kış mevsiminde, soğuk hava koşullarında sıcaklık sağlarken aynı zamanda stil sahibi bir görünüm sunar. Ayrıca, desenleri sayesinde çeşitli kombinler oluşturulabilir.</li>
+        <li><strong>Stil ve Moda:</strong> Jakarlı kazaklar, hem gündelik hem de özel günlerde kullanılabilir. Farklı desen ve renk seçenekleri ile çeşitli stillere uyum sağlayabilirler.</li>
+    </ul>
+
+    <h2>Sıklıkla Kullanılan Eşanlamlı Kelimeler:</h2>
+    <ul>
+        <li><strong>Jacquard Sweater (İngilizce):</strong> Jakarlı kazakların İngilizce karşılığıdır.</li>
+    </ul>`,
+    alt: 'Jakarlı kazak',
+    link: 'kadin-kazak-cesitleri/jakarli',
+    src: 'kadin-kazak/jakarli-kazak.jpg'
 }
 ]
 
