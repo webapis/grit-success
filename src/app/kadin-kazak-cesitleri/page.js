210f2a1f696719497eb9c0a820a4026ea99807f4
Serdar Ashirov <webapis@users.noreply.github.com>
2024-09-03 08:56:05 +0500

    Update page.js

--- src/app/kadin-kazak-cesitleri/page.js
+++ src/app/kadin-kazak-cesitleri/page.js
@@ -4,17 +4,18 @@ import { Grid, Container, Typography } from "@mui/material"
 import PersistentDrawerLeft from "./components/AppBarComp"
 
 
-export async function generateMetadata({ params: { slug } }) {
+export async function generateMetadata() {
 
 
     return {
         title: "kazak yaka çeşitleri",
-        description: "Kazak çeşitleri, Kazak yaka çeşitleri",
+        description: "Salaş kazak, Baklava Desenli Kazak, Crop kazak ve diğer kazakların  çeşitleri, Kazak yaka çeşitleri, Kazak örnekleri",
+        keywords:"Balıkçı Yaka Kazak, Bisiklet Yaka Kazak, Şal Yaka Kazak, Polo Yaka Kazak, V Yaka Kazak, Aran Kazak, Kazak Yelek, Ajurlu Kazak"
      
 
     }
 }
 export default function KadinKazak() {
 
-    return <PersistentDrawerLeft> <Container sx={{ marginTop: 3 }}><Typography variant="h4" >Kazak Çeşitleri</Typography> <Grid container spacing={1} >{blogBoxData.map(m => <Grid item xs={12}><BlogBox {...m} /> </Grid>)}</Grid> </Container></PersistentDrawerLeft>
+    return <PersistentDrawerLeft> <Container sx={{ marginTop: 3 }}><Typography variant="h4" >Kazak Çeşitleri</Typography> <Grid container spacing={1} >{blogBoxData.map((m,i) => <Grid item xs={12} key={i}><BlogBox {...m} /> </Grid>)}</Grid> </Container></PersistentDrawerLeft>
 }
