3d1533630f27a0e4882abd816d06cc75886cfe65
Serdar Ashirov <webapis@users.noreply.github.com>
2024-09-24 10:10:45 +0500

    т

--- src/app/dizikiyafeti/comps/Image.js
+++ src/app/dizikiyafeti/comps/Image.js
@@ -6,7 +6,7 @@ import  Box  from '@mui/material/Box';
 function Image({  fotografUrl,  alt }) {
 
   return (
-<Box sx={{ borderRadius:10,paddingRight:2,paddingLeft:2, border: {xs:"",md:"2px solid"}, borderColor:{xs:"none",md:"#e0e0e0"}, padding: {xs:0,md:1}}}>
+<Box sx={{ borderRadius:10,paddingRight:2,paddingLeft:2, padding: {xs:0,md:1}}}>
     <img
     src={fotografUrl}
 
