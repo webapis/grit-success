dda6b3ae5759b0b917876c41b54dbe04733cbdad
Serdar Ashirov <webapis@users.noreply.github.com>
2024-10-24 13:04:04 +0500

    Update BreadcrumbsComponent.js

--- src/app/sponsor-giyim/components/BreadcrumbsComponent.js
+++ src/app/sponsor-giyim/components/BreadcrumbsComponent.js
@@ -1,4 +1,3 @@
-// BreadcrumbsComponent.js
 import React from 'react';
 import { Breadcrumbs, Link, Typography } from '@mui/material';
 
@@ -10,13 +9,18 @@ const BreadcrumbsComponent = ({ urlPath }) => {
     <div style={{ margin: '20px' }}>
       <Breadcrumbs aria-label="breadcrumb">
         {segments.map((segment, index) => {
-          const path = segments.slice(0, index + 1).join('/').replace(' ','-');
-          return index === segments.length - 1 ? (
-            <Typography key={index} color="textPrimary">{segment}</Typography>
-          ) : (
-            <Link key={index} href={`${path}`} color="inherit">
-              {segment}
-            </Link>
+          const path = segments.slice(0, index + 1).join('/').replace(' ', '-');
+          const isLastSegment = index === segments.length - 1;
+          const isMixed = /[a-zA-Z]/.test(segment) && /\d/.test(segment); // Check if segment contains both letters and numbers
+
+          return isLastSegment && isMixed ? null : (
+            isLastSegment ? (
+              <Typography key={index} color="textPrimary">{segment}</Typography>
+            ) : (
+              <Link key={index} href={`${path}`} color="inherit">
+                {segment}
+              </Link>
+            )
           );
         })}
       </Breadcrumbs>
