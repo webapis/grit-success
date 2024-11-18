7fb2fe6aae24ed90538f28be6034032d81835744
Serdar Ashirov <webapis@users.noreply.github.com>
2024-11-18 11:23:36 +0500

    refactor

--- src/app/dizi/[...slug]/page.js
+++ src/app/dizi/[...slug]/page.js
@@ -5,7 +5,7 @@ import SearchResultContainer from '../../dizisponsoru/comp/SearchResultContainer
 
 import PaginationContainer from '@/app/dizisponsoru/comp/PaginationContainer';
 //import path from 'path'
-import Fuse from 'fuse.js'
+//import Fuse from 'fuse.js'
 import pagesMetaData from '@/app/dizi/pageMetadata.json'
 import pagesData from '@/app/dizi/dizisponsoru.json'
 import deaccent from '@/app/dizisponsoru/[...slug]/deaccent';
@@ -13,7 +13,7 @@ import deaccent from '@/app/dizisponsoru/[...slug]/deaccent';
 
 
 
-export  function generateMetadata({ params }) {
+export function generateMetadata({ params }) {
 
     const result = pagesMetaData.find(f => {
         const current = f.slug
@@ -39,18 +39,12 @@ export  function generateMetadata({ params }) {
     }
 
 
-
-
-
-
-
-
 }
 
 
 
-export default  function DiziPage({ params }) {
-    
+export default function DiziPage({ params }) {
+
     const page = params.slug[2] ? parseInt(params.slug[2]) : 1
 
 
@@ -63,21 +57,20 @@ export default  function DiziPage({ params }) {
         return match
     })
 
-    const fuse = new Fuse(pagesData, { keys: ['ServiceName', 'TVSeriesTitle', 'Tag', 'Name', 'tag'], minMatchCharLength: 4, threshold: 0.0  })
 
 
-    if (result) {
-        const { pageTitle, search } = result
-        let results = fuse.search(search)
-        const paginatedData = paginate(results, page, 50)
-        const pageCount = Math.ceil(results.length / 50)
-        
+    const resultSimple = pagesData.filter((f) => f.tag === result.slug.replace('-dizi-sponsorlari', ''))
+
+
+        const { pageTitle } = result
+
+        const paginatedData = paginate(resultSimple, page, 50)
+        const pageCount = Math.ceil(resultSimple.length / 50)
+
         return <> <SearchResultContainer data={paginatedData} pageTitle={pageTitle} dizi={deaccent(result.dizi).replaceAll(' ', '-').toLowerCase()} keyword="tum" />
             <PaginationContainer count={pageCount} page={page} url={`/dizi/${params.slug[0]}/sayfa/`} />
         </>
-    } else {
-        return <div>Loading....</div>
-    }
+ 
 
 
 
@@ -95,29 +88,31 @@ function paginate(array, page, pageSize) {
 }
 
 
-export  function generateStaticParams() {
+export function generateStaticParams() {
 
     const pageCandidates = []
     for (let meta of pagesMetaData) {
 
-        const fuse = new Fuse(pagesData, { keys: ['ServiceName', 'TVSeriesTitle', 'Tag', 'Name', 'tag'],  minMatchCharLength: 0, threshold: 0.3 })
-        const { search } = meta
-        let results = fuse.search(search)
-        const pageCount = Math.ceil(results.length / 25)
+       // const fuse = new Fuse(pagesData, { keys: ['ServiceName', 'TVSeriesTitle', 'Tag', 'Name', 'tag'], minMatchCharLength: 0, threshold: 0.3 })
+        const { search,slug } = meta
+
+        const resultSimple = pagesData.filter((f) => f.tag === slug.replace('-dizi-sponsorlari', ''))
+        //let results = fuse.search(search)
+        const pageCount = Math.ceil(resultSimple.length / 50)
 
         pageCandidates.push({ pageCount, slug: meta.slug })
 
- 
+
     }
 
     const pages = flattenArrayByPageCount(pageCandidates)
 
     return pages.map((post) => {
         const { slug, page } = post
-        if(page===1){
+        if (page === 1) {
             return {
                 slug: [slug]
-            }  
+            }
         }
         return {
             slug: [slug, 'sayfa', page.toString()]
